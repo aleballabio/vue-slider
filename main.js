39b34e94108aab8da9a8129b68534a8b4eb441d4
aleballabio <aleballabio05@gmail.com>
2022-03-11 15:27:37 +0100

    data set

---
+++ main.js
@@ -0,0 +1,40 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        ActiveIndex: 0,
+        arrSlides: [
+            {
+                name: 'Svezia',
+                image: '01.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+
+            {
+                name: 'Svizzera',
+                image: '02.jpg',
+                text: 'Lorem ipsum'
+            },
+
+            {
+                name: 'Gran Bretagna',
+                image: '03.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+            },
+
+            {
+                name: 'Germania',
+                image: '04.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            },
+
+            {
+                name: 'Paradise',
+                image: '05.jpg',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            }
+        ]
+    }
+    methods: {
+
+    }
+})
